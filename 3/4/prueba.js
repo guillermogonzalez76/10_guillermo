b11467ed216f824e5bd83c1aea7ff8ad3d39e223
guillermogonzalez76 <guillermo.gonzalez76@educa.madrid.org>
2025-12-05 13:48:18 +0100

    javascript

---
+++ 3/4/prueba.js
@@ -0,0 +1,13 @@
+      //soy un comentario de una linea
+        /* soy un comentario con varias lineas
+        */
+       var mensaje = "Hola mundo que tal"
+       console.log(mensaje);
+       console.log(document);
+       alert ('prueba de codigo');
+       var esviernes =true;
+       console.log("Es viernes y queremos irnos:" + esviernes);
+       //debuger
+       if (esviernes == true){
+        console.log("Por fin es viernes vete a la meir Xulei")
+       }
